92b6f2f227f46af43132ad4f770d8f691742ac4f
Kevin Leung <leung.kevin23@gmail.com>
2022-05-22 21:05:01 -0400

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * Proud owner of jeremy-de-la-cruz NPM package. All rights reserved.
+ */
+
+// const kevinLeung = require('kevin-leung');
+import kevinLeung from "kevin-leung";
+
+// Do something not so impressive
+const jeremyDLC = () => {
+  return kevinLeung() + ' Jeremy too.';
+};
+  
+export default jeremyDLC;
+  
